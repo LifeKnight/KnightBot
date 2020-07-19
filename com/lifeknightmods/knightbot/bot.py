6aa89072fd94030b89b953914fe5d204d4d290bc
thera <therandusstguy@gmail.com>
2020-07-19 17:12:10 -0400

    more stuff. fixed. gives pointest roole. scrambeld words. maybe amth in the future.

--- com/lifeknightmods/knightbot/bot.py
+++ com/lifeknightmods/knightbot/bot.py
@@ -1,7 +1,12 @@
+import asyncio
 import json
 import os
 import sys
 import time
+import urllib.request
+import schedule
+import threading
+import random
 
 import discord
 from dotenv import load_dotenv
@@ -16,8 +21,75 @@ prefix = '/'
 discord_server_members = []
 last_config_read_time = None
 
+knightbot_channel = None
+
+knightbot_response_channel = None
+
+english_word_list = []
+
+last_scrambled_word = None
+
+last_scrambled_time = 0
+
 
 class Utilities:
+
+    @staticmethod
+    async def on_startup():
+        await Utilities.read_configuration_file()
+        DiscordServerMember.check_for_member_updates()
+        await Utilities.update_configuration_file()
+        page = urllib.request.urlopen("http://www.desiquintans.com/downloads/nounlist/nounlist.txt")
+        global english_word_list
+        for line in page:
+            if len(line) >= 5:
+                as_string = str(line)
+                as_string = as_string[2:len(as_string) - 3]
+                english_word_list.append(as_string)
+
+        page = urllib.request.urlopen("https://gist.githubusercontent.com/hugsy/8910dc78d208e40de42deb29e62df913/raw"
+                                      "/eec99c5597a73f6a9240cab26965a8609fa0f6ea/english-adjectives.txt")
+        for line in page:
+            if len(line) >= 5:
+                as_string = str(line)
+                as_string = as_string[2:len(as_string) - 3]
+                english_word_list.append(as_string)
+
+    @staticmethod
+    def get_current_time_millis():
+        return int(round(time.time() * 1000))
+
+    @staticmethod
+    async def send_scrambled_message():
+        global english_word_list
+        random_word = english_word_list[random.randint(0, len(english_word_list) - 1)]
+        global last_scrambled_word
+        last_scrambled_word = random_word
+        scrambled = Utilities.scramble_word(random_word)
+        await get_knightbot_channel().send(
+            f"Unscramble the message to receive points! **{scrambled}**")
+        print(f"Scrambled word: {random_word}")
+        global last_scrambled_time
+        last_scrambled_time = Utilities.get_current_time_millis()
+
+    @staticmethod
+    def scramble_word(word):
+        new_word = ""
+        indexes_used = []
+
+        while len(new_word) != len(word):
+            random_index = Utilities.get_random_number_that_isnt(0, len(word) - 1, indexes_used)
+            new_word += word[random_index]
+            indexes_used.append(random_index)
+
+        return new_word
+
+    @staticmethod
+    def get_random_number_that_isnt(min, max, previous):
+        random_value = random.randint(min, max)
+        if not random_value in previous:
+            return random_value
+        return Utilities.get_random_number_that_isnt(min, max, previous)
 
     @staticmethod
     async def read_configuration_file():
@@ -36,12 +108,12 @@ class Utilities:
         try:
             with open("configuration.txt", 'w') as file:
                 data = {
-                    "time": int(round(time.time() * 1000)),
+                    "time": Utilities.get_current_time_millis(),
                     "members": DiscordServerMember.get_members_as_tuple(),
                 }
                 file.write(json.dumps(data, indent=4))
         except:
-            await on_error("An error occurred while trying to write to the configuration file/")
+            await on_error("An error occurred while trying to write to the configuration file.")
 
     @staticmethod
     async def exit_bot():
@@ -50,23 +122,35 @@ class Utilities:
 
 
 class DiscordServerMember:
-    def __init__(self, id_in):
+    def __init__(self, id_in, points_in):
         self.id = id_in
+        self.points = points_in
+        if self.points is None:
+            self.points = 0
+
+        for member in get_guild().members:
+            if member.id == self.id:
+                self.user = member
+                break
         discord_server_members.append(self)
+
+    def get_user(self):
+        return self.user
 
     def get_id(self):
         return self.id
 
     def get_name(self):
-        for member in get_guild().members:
-            if member.id == self.id:
-                return member.name
+        return self.user.name
+
+    def get_points(self):
+        return self.points
 
     @staticmethod
     def check_for_member_updates():
         for member in get_guild().members:
             if not DiscordServerMember.has_member(member.id):
-                DiscordServerMember(member.id)
+                DiscordServerMember(member.id, 0)
 
     @staticmethod
     def has_member(id_in):
@@ -76,13 +160,35 @@ class DiscordServerMember:
     def get_members_as_tuple():
         members = []
         for member in discord_server_members:
-            members.append({"id": member.get_id()})
+            members.append({"id": member.id, "points": member.points})
         return members
 
     @staticmethod
     def read_members_from_tuple(members):
-        for member_string in members:
-            DiscordServerMember(member_string["id"])
+        for member_map in members:
+            DiscordServerMember(member_map["id"], member_map["points"])
+
+    @staticmethod
+    def get_by_id_or_name(id_or_name):
+        for member in discord_server_members:
+            if member.id == id_or_name:
+                return member
+        for member in discord_server_members:
+            if member.get_name().lower() == id_or_name.lower() or member.get_user().displayname == id_or_name.lower():
+                return member
+            
+    @staticmethod
+    def add_points_by_id(id_in, points):
+        DiscordServerMember.get_by_id_or_name(id_in).points += points
+
+    @staticmethod
+    def get_member_with_most_points():
+        member_with_most_points = None
+        for member in discord_server_members:
+            if member_with_most_points is None or member.get_points() > member_with_most_points.get_points():
+                member_with_most_points = member
+
+        return member_with_most_points
 
 
 @client.event
@@ -91,10 +197,8 @@ async def on_ready():
         f'{client.user} is connected to the following guild:\n'
         f'{get_guild().name} (id: {get_guild().id})'
     )
-
-    await Utilities.read_configuration_file()
-    DiscordServerMember.check_for_member_updates()
-    await Utilities.update_configuration_file()
+    await Utilities.on_startup()
+    await Utilities.send_scrambled_message()
 
 
 @client.event
@@ -116,11 +220,41 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if len(message.content) > 1 and message.content[0] == prefix:
-        await process_command(message.channel, message.content[1:])
+    if message.channel.id == get_knightbot_response_channel().id:
+        await process_response(message)
+
+    elif len(message.content) > 1 and message.content[0] == prefix:
+        await process_command(message, message.content[1:])
+
+    if last_scrambled_time == 0 or last_scrambled_time - Utilities.get_current_time_millis() < -15 * 60 * 1000:
+        await Utilities.send_scrambled_message()
 
 
-async def process_command(channel, arguments):
+async def process_response(message):
+    if message.content.lower() == last_scrambled_word:
+        random_points = random.randint(1, 3)
+        if random_points == 1:
+            point_message = "You received **1 point!**"
+        else:
+            point_message = f"You received **{random_points} points!**"
+        DiscordServerMember.add_points_by_id(message.author.id, random_points)
+        await get_knightbot_response_channel().send(f"Nice <@{message.author.id}>! {point_message}")
+
+        top_member_id = DiscordServerMember.get_member_with_most_points().get_id()
+        for member in discord_server_members:
+            user = member.get_user()
+            if user.roles.__contains__(get_pointest_role()) and user.id != top_member_id:
+                await user.remove_roles(get_pointest_role())
+            elif not user.roles.__contains__(get_pointest_role()) and user.id == top_member_id:
+                await user.add_roles(get_pointest_role())
+                await get_knightbot_response_channel().send(f"Congratulations! You are now the POINTEST!")
+
+        await Utilities.send_scrambled_message()
+        await Utilities.update_configuration_file()
+        return
+
+
+async def process_command(message, arguments):
     text_response = None
     embed_response = None
     if arguments.lower() == "social" or arguments.lower() == "socials":
@@ -145,7 +279,8 @@ async def process_command(channel, arguments):
         for i in mods:
             mods_embed.add_field(name=i, value=mods[i], inline=False)
         embed_response = mods_embed
-    elif arguments.lower().replace(" ", "") == "texturepacks" or arguments.lower().replace(" ", "") == "packs":
+    elif arguments.lower().replace(" ", "") == "texturepacks" or arguments.lower().replace(" ",
+                                                                                           "") == "resourcepacks" or arguments.lower() == "packs":
         resource_packs = {
             "16x Swords, 128x Blocks": "http://www.mediafire.com/file/v20bu2xzzgtyzlf/A_Simple_PvP_Experience%2521_"
                                        "%255BRed_Diamond_Edit%255D.zip/file",
@@ -158,6 +293,28 @@ async def process_command(channel, arguments):
         for i in resource_packs:
             resource_packs_embed.add_field(name=i, value=resource_packs[i], inline=False)
         embed_response = resource_packs_embed
+    elif arguments.lower().startswith("profile"):
+        if len(arguments.replace(" ", "")) == 7:
+            member_profile_embed = discord.Embed(title=f"{message.author.name}", description="Your KnightBot profile.",
+                                                 color=0xff0000)
+            member = DiscordServerMember.get_by_id_or_name(message.author.id)
+            member_profile_embed.add_field(name="Points", value=str(member.get_points()), inline=True)
+            embed_response = member_profile_embed
+        else:
+            try:
+                member_id_or_name = arguments.split(" ")[1]
+                if member_id_or_name.startswith("<@"):
+                    member_id_or_name = int(member_id_or_name[2:len(member_id_or_name) - 1])
+                elif member_id_or_name.isnumeric():
+                    member_id_or_name = int(member_id_or_name)
+
+                member = DiscordServerMember.get_by_id_or_name(member_id_or_name)
+                member_profile_embed = discord.Embed(title=member.get_name(), description=f"<@{member.get_id()}>'s KnightBot profile.",
+                                                 color=0xff0000)
+                member_profile_embed.add_field(name="Points", value=str(member.get_points()), inline=True)
+                embed_response = member_profile_embed
+            except:
+                text_response = "No user found."
     else:
         commands = {
             "/socials": "Lists LifeKnight's accounts.",
@@ -170,18 +327,33 @@ async def process_command(channel, arguments):
         embed_response = commands_embed
 
     if text_response is not None:
-        await channel.send(text_response)
+        await message.channel.send(text_response)
     elif embed_response is not None:
-        await channel.send(embed=embed_response)
+        await message.channel.send(embed=embed_response)
 
 
 async def on_error(event, *args, **kwargs):
+    print(f'There was an error: {event} - {args} - {kwargs}\n')
     with open('err.log', 'a') as f:
         f.write(f'There was an error: {event} - {args} - {kwargs}\n')
 
 
 def get_guild():
     return discord.utils.get(client.guilds, id=366700898602188811)
+
+
+def get_knightbot_channel():
+    return discord.utils.get(get_guild().channels, id=734409425430773850)
+
+
+def get_knightbot_response_channel():
+    return discord.utils.get(get_guild().channels, id=734413328075587644)
+
+
+def get_pointest_role():
+    for role in get_guild().roles:
+        if role.id == 734507294980571258:
+            return role
 
 
 client.run(TOKEN)
@@ -192,5 +364,5 @@ IDEAS
 /up, gives you points, person with most up gets the up role
     - role editing
     - perhaps you have to wait a random amount of time and if you wait to long or too short you will receive a penalty
-
+    - unscramble, a channel for the bot to send messages
 """
